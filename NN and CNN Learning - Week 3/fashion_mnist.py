013e4d24b9e98c1620b535ba58db9bbec57a03c9
HarishKongari <36798369+HarishKongari@users.noreply.github.com>
2019-11-14 02:03:08 +0100

    Update fashion_mnist.py

--- NN and CNN Learning - Week 3/fashion_mnist.py
+++ NN and CNN Learning - Week 3/fashion_mnist.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.preprocessing.image import random_rotation, random_shear, random_shift, random_zoom
 
 mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -12,21 +13,19 @@ print(np.shape(train_images),
 np.shape(test_images),
 np.shape(train_labels),
 np.shape(test_labels))
-
-
-'''data = tf.data.Dataset.from_tensor_slices(
-    (train_images.reshape([-1, 784]).astype(np.float32) / 255, train_labels.astype(np.int32)))
-data = data.shuffle(buffer_size=60000).batch(128).repeat()
-# note: we batch the test data, but do not shuffle/repeat
-test_data = tf.data.Dataset.from_tensor_slices(
-    (test_images.reshape([-1, 784]).astype(np.float32) / 255, test_labels.astype(np.int32))).batch(128)'''
-
+        
 tuple_inputsize = (28, 28, 1)
 train_images = train_images.reshape(-1, 28, 28, 1)
 test_images = test_images.reshape(-1, 28, 28, 1)
-n_h = 512
 
-layer_list = [tf.keras.layers.Convolution2D(filters = 28, kernel_size = (3, 3),
+layer_list = [tf.keras.layers.Convolution2D(filters = 32, kernel_size = (3, 3),
+                               padding = 'same',input_shape = tuple_inputsize,
+                               kernel_initializer = 
+                               tf.initializers.RandomNormal(mean = 0.0,
+                                stddev = 0.05, seed = None),
+                             bias_initializer=tf.initializers.constant(0.001)),
+    tf.keras.layers.MaxPooling2D(pool_size = (2,2)),
+    tf.keras.layers.Convolution2D(filters = 28 , kernel_size = (3, 3),
                                padding = 'same',input_shape = tuple_inputsize,
                                kernel_initializer = 
                                tf.initializers.RandomNormal(mean = 0.0,
@@ -34,123 +33,88 @@ layer_list = [tf.keras.layers.Convolution2D(filters = 28, kernel_size = (3, 3),
                              bias_initializer=tf.initializers.constant(0.001)),
     tf.keras.layers.MaxPooling2D(pool_size = (2,2)),
     tf.keras.layers.Flatten(),
-              tf.keras.layers.Dense(n_h, activation=tf.nn.relu,
+              tf.keras.layers.Dense(686, activation=tf.nn.relu,
                            kernel_initializer = 
                                tf.initializers.RandomNormal(mean = 0.0,
                                 stddev = 0.05, seed = None),
                              bias_initializer=tf.initializers.constant(0.001)),
-              tf.keras.layers.Dense(n_h/2, activation=tf.nn.relu,
+              tf.keras.layers.Dense(686/2, activation=tf.nn.relu,
                                     kernel_initializer = 
                                tf.initializers.RandomNormal(mean = 0.0,
                                 stddev = 0.05, seed = None),
                              bias_initializer=tf.initializers.constant(0.001)),
-              tf.keras.layers.Dense(n_h/4, activation=tf.nn.relu,
+              tf.keras.layers.Dense(686/4, activation=tf.nn.relu,
                                     kernel_initializer = 
                                tf.initializers.RandomNormal(mean = 0.0,
                                 stddev = 0.05, seed = None),
                              bias_initializer=tf.initializers.constant(0.001)),
-              tf.keras.layers.Dense(10, activation = tf.nn.softmax)] # default is no activation
-
-'''layer_list = [tf.keras.layers.Convolution2D(filters = 64, kernel_size = (3, 3),
-                               padding = 'same',
-                               kernel_initializer = 
-                               tf.initializers.RandomNormal(mean = 0.0,
-                                stddev = 0.05, seed = None),
-                             bias_initializer=tf.initializers.constant(0.001)),
-    tf.keras.layers.MaxPooling2D(pool_size = (2,2)),
-    tf.keras.layers.Convolution2D(filters = 32, kernel_size = (3, 3),
-                               padding = 'same',
-                               kernel_initializer = 
-                               tf.initializers.RandomNormal(mean = 0.0,
-                                stddev = 0.05, seed = None),
-                             bias_initializer=tf.initializers.constant(0.001)),
-    tf.keras.layers.MaxPooling2D(pool_size = (2,2)),
-    tf.keras.layers.Flatten(),tf.keras.layers.Dense(n_h, activation=tf.nn.relu,
-                           kernel_initializer = 
-                               tf.initializers.RandomNormal(mean = 0.0,
-                                stddev = 0.05, seed = None),
-                             bias_initializer=tf.initializers.constant(0.001)),
-              tf.keras.layers.Dense(n_h/2, activation=tf.nn.relu,
-                                    kernel_initializer = 
-                               tf.initializers.RandomNormal(mean = 0.0,
-                                stddev = 0.05, seed = None),
-                             bias_initializer=tf.initializers.constant(0.001)),
-              tf.keras.layers.Dense(10, activation = tf.nn.softmax)]'''
+              tf.keras.layers.Dense(10, activation = tf.nn.softmax)]
 
 model = tf.keras.Sequential(layer_list)
 
-#model.build((None, 784))  # optional -- note None for the batch axis!!
-
-
-
-
-
-
 train_images = train_images/255.0
 test_images = test_images/255.0
-'''train_images = np.asarray(train_images, dtype=np.float64) 
-test_images = np.asarray(test_images, dtype=np.float64)'''
-'''train_images = train_images.reshape(-1, 28, 28, 1)
-test_images = test_images.reshape(-1, 28, 28, 1)'''
 
+augmented_image = []
+augmented_image_labels = []
+
+for num in range (0, train_images.shape[0]):
+
+	for i in range(0, 1):
+			# original image:
+		augmented_image.append(train_images[num])
+		augmented_image_labels.append(train_labels[num])
+
+		if True:
+			augmented_image.append(tf.keras.preprocessing.image.random_rotation(train_images[num], 20, row_axis=0, col_axis=1, channel_axis=2))
+			augmented_image_labels.append(train_labels[num])
+
+		if True:
+			augmented_image.append(tf.keras.preprocessing.image.random_shear(train_images[num], 0.2, row_axis=0, col_axis=1, channel_axis=2))
+			augmented_image_labels.append(train_labels[num])
+
+		if True:
+			augmented_image.append(tf.keras.preprocessing.image.random_shift(train_images[num], 0.2, 0.2, row_axis=0, col_axis=1, channel_axis=2))
+			augmented_image_labels.append(train_labels[num])
+
+		if True:
+			augmented_image.append(tf.keras.preprocessing.image.random_zoom(train_images[num], (0.7, 0.7), row_axis=0, col_axis=1, channel_axis=2))
+			augmented_image_labels.append(train_labels[num])
+
+
+
+augmented_image = tf.convert_to_tensor(augmented_image)
+
+augmented_image_labels = tf.convert_to_tensor(augmented_image_labels)
+
+import numpy as np
+augmented_image = np.asarray(augmented_image).reshape((-1, 28, 28, 1))
+
+augmented_image_labels =  np.array(augmented_image_labels)
+
+augmented_image_labels =  augmented_image_labels.reshape(120000,1)
+
+#train_img_gen = datagen.flow(train_images, train_labels, batch_size=60)
+tf.data.Dataset.from_tensor_slices(augmented_image)
+tf.data.Dataset.from_tensor_slices(augmented_image_labels)
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy', 'Precision', 'Recall'])
 
+model.fit(augmented_image, augmented_image_labels, epochs = 3,
+          validation_data = (test_images, test_labels), callbacks=callbacks)
+model.evaluate(test_images, test_labels)
+model.predict(test_images)
 
-model_history = model.fit(train_images, train_labels, epochs = 50, batch_size = 512, 
-          validation_data = (test_images, test_labels))
 model.summary()
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2, batch_size = 256)
+import pandas as pd
+classes=np.arange(0, 10, 1)
+y_pred=model.predict_classes(test_images)
+con_mat = tf.math.confusion_matrix(labels=test_labels, predictions=y_pred).numpy()
 
-print('\n Test accuracy:', test_acc)
-
-'''for step, (img_batch, lbl_batch) in enumerate(data):
-    if step > train_steps:
-        break
-    with tf.GradientTape() as tape:
-        logits = model(img_batch)
-        # loss format is generally: first argument targets, second argument outputs
-        xent = loss_fn(lbl_batch, logits)
-    # if you didn't build the model, it is important that you get the variables
-    # AFTER the model has been called the first time
-    varis = model.trainable_variables
-    grads = tape.gradient(xent, varis)
-      
-    opt.apply_gradients(zip(grads, varis))
-    
-    train_acc_metric(lbl_batch, logits)
-    
-    if not step % 100:
-        # this is different from before. there, we only evaluated accuracy
-        # for one batch. Now, we always average over 100 batches
-        print("Loss: {} Accuracy: {}".format(xent, train_acc_metric.result()))
-        train_acc_metric.reset_states()'''
-
-
-
-
-# this is very convenient -- before, we usually had code that
-# evaluates the whole test set at once -- this won't work for
-# large datasets/models. With metrics, we can just iterate
-# over the data and the metric takes care of averaging etc.
-
-'''test_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
-for img_batch, lbl_batch in test_data:
-    test_acc_metric(lbl_batch, model(img_batch))
-print("Test acc: {}".format(test_acc_metric.result()))'''  
-
-
-'''def plot_history(histories, key='binary_crossentropy'):
-  plt.figure(figsize=(16,10))
-  for name, history in histories:
-    val = plt.plot(history.epoch, history.history['val_'+key],
-                   '--', label=name.title()+' Val')
-    plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
-             label=name.title()+' Train')
-  plt.xlabel('Epochs')
-  plt.ylabel(key.replace('_',' ').title())
-  plt.legend()
-  plt.xlim([0,max(history.epoch)])
-plot_history('model', model_history)'''
+con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
+ 
+con_mat_df = pd.DataFrame(con_mat_norm,
+                     index = classes, 
+                     columns = classes)
